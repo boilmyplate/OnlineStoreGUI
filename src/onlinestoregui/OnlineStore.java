b1d9e6883eb6a0c792ce6c2fbe66f00ba86035bd
petersun <petersun@localhost>
2024-06-06 11:57:23 +1200

    phase 2

--- src/onlinestoregui/OnlineStore.java
+++ src/onlinestoregui/OnlineStore.java
@@ -3,17 +3,52 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package onlinestoregui;
-
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author petersun
  */
-public interface OnlineStore {
-    void displayWelcomeMessage();
-    void displayProducts();
-    Product chooseProduct(int productId);
-    double checkout(Product product, int quantity);
-    void saveSaleRecord(SaleRecord record);
-    String getSaleRecordAsString(int index);
-    boolean isValidProduct(int productId);
+public class OnlineStore {
+    private List<Product> products;
+    private Cart cart;
+
+    public OnlineStore() {
+        products = new ArrayList<>();
+        cart = new Cart();
+        System.out.println("OnlineStore initialized.");
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+        System.out.println("Product added: " + product.getName());
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addToCart(Product product, int quantity) {
+        cart.addItem(new CartItem(product, quantity));
+        System.out.println("Added to cart: " + quantity + " x " + product.getName());
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void saveRecords(String customerName) {
+        SaleRecord saleRecord = new SaleRecord(cart.getItems(), customerName);
+        saleRecord.save();
+        System.out.println("Records saved for customer: " + customerName);
+    }
+
+    public boolean isValidProduct(int index) {
+        return index >= 0 && index < products.size();
+    }
+
+    public void clearCart() {
+        cart.clearItems();
+        System.out.println("Cart cleared.");
+    }
 }
